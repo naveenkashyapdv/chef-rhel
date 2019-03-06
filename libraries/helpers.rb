1ecfee433e9b4da876e226e1f8fc146a48533789
naveenkashyapdv <d.naveenkashyap@gmail.com>
2019-03-06 15:21:20 -0800

    Update helpers.rb

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -32,7 +32,7 @@ module Rhel
       Chef::Log.debug("Running command #{command} as root")
 
       shellout = Mixlib::ShellOut.new(command, user: 'root').run_command
-      unless shellout.stderr.empty? || shellout.stderr.registered?
+      unless shellout.stderr.empty? || registered_with_rhsm?
         Chef::Application.fatal! "subscription-manager returned error #{shellout.stderr}"
       end
     end
